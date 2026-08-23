9b2462b1570d95bbe842ca4a40a16040022ec87d
MohnishArora <rtrmohnish@gmail.com>
2026-08-22 23:32:41 -0500

    chore(dagster): updating app.py with updates until sensor, cron schedule, and archive daily practice scripts

--- practice_logs/22ndAug.py
+++ practice_logs/22ndAug.py
@@ -1,15 +1,12 @@
 import os
-from dagster import ( 
-                    asset, MaterializeResult, 
-                    asset_check, AssetCheckResult, 
-                    Definitions, 
-                    define_asset_job, ScheduleDefinition,
-                    RunRequest, SkipReason, sensor
-                    )
+from dagster import (asset, MaterializeResult,
+                     asset_check, AssetCheckResult,
+                     Definitions,
+                     define_asset_job, ScheduleDefinition,
+                    sensor, RunRequest, SkipReason
+                     )
 
 FILE_PATH = "incoming_data.csv"
-
-#Step 1: Create Assets 
 @asset
 def raw_sales():
     if os.path.exists(FILE_PATH):
@@ -18,42 +15,37 @@ def raw_sales():
             data = [int(x.strip()) for x in lines if x.strip()]
     else:
         data = [100, 250, 75, 400]
-
     return MaterializeResult(
         value = data,
         metadata = {"total_records": len(data),
-                    "max_sale": max(data)}
+                    "max_sales": max(data)}
     )
+
+
 
 @asset
 def total_revenue(raw_sales):
     revenue = sum(raw_sales)
-
     return MaterializeResult(
         value = revenue,
-        metadata = {"total_revenue": revenue, 
-                    "order_count": len(raw_sales)}
+        metadata = {"total_revenue": revenue,
+                    "Order_count": len(raw_sales)}
     )
 
-
-# Step2: Add check: total revenue should be more than 500
+# Asset Check Creation
 @asset_check(asset=total_revenue)
 def check_min_revenue(total_revenue):
-    
     min_revenue = total_revenue >= 500
-
     return AssetCheckResult(
         passed = min_revenue,
-        metadata = {"threshold": 500, 
-                    "value": total_revenue}
+        metadata = {
+            "threshold": 500,
+            "value": total_revenue
+        }
     )
 
-# Step 3: Add Definitions. Bundle assets and checks together for Dagster to differentiate. 
-# Pushed after Sensor
 
-
-
-# Step 4.a.: Add a Schedule.
+# Schedule Creation
 daily_sales_job = define_asset_job(
     name = "daily_sales_job",
     selection = ["raw_sales", "total_revenue"]
@@ -65,17 +57,17 @@ daily_sales_schedule = ScheduleDefinition(
     execution_timezone = "America/Chicago"
 )
 
-# Step 4.b.: Add a Sensor.
+# Sensor Creation
 @sensor(job=daily_sales_job)
 def new_file_sensor():
-    file_path = "incoming_data.csv"
+    file_path = FILE_PATH
     
     if os.path.exists(file_path):
         yield RunRequest(run_key=None)
     else:
         yield SkipReason(f"Waiting for {file_path} to arrive.")
 
-
+#Definitions Creation
 defs = Definitions(
     assets = [raw_sales, total_revenue],
     asset_checks = [check_min_revenue],
